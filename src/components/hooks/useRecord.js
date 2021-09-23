81fc05a0c5c8a3cef77d9a70ab1a1f4670359b52
Elise Muellerleile <eliamue@live.com>
2021-09-22 20:21:03 -0500

    index consts added, useRecord const fixed to now reflect index in history and for undo button, undo button now works

--- src/components/hooks/useRecord.js
+++ src/components/hooks/useRecord.js
@@ -2,23 +2,30 @@ import { useEffect, useState } from 'react';
 
 export const useRecord = (initialColor = '#541812') => {
   const [current, setCurrent] = useState(initialColor);
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState([initialColor]);
+  const [index, setIndex] = useState(0);
+
   const record = (newColor) => {
+    setHistory((prev) => [
+      ...prev.slice(0, index + 1),
+      newColor,
+      ...prev.slice(index + 1),
+    ]);
     setCurrent(newColor);
+    console.log(history);
   };
-  
+
   const undo = () => {
-
+    setCurrent(history[index - 1]);
   };
-    
-  const redo = () => {
 
-  };
+  const redo = () => {};
 
   useEffect(() => {
-    setHistory((prev) => [...prev, current]);
-    console.log('-----history-----', history);
+    setIndex(history.indexOf(current));
   }, [current]);
+  //     setHistory((prev) => [...prev, current]);
+  //   }, [current]);
 
-  return { undo, redo, current, record };
+  return { undo, redo, current, record, history, index };
 };
